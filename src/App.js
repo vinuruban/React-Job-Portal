a4d399537014bbb8e45ac0aa73a7d06a712b16f4
Shantharoopan <vinu.ruban@netbuilder.com>
2019-10-11 15:02:49 +0100

    day 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import Table from './Table'
-// To import the Table comp in this class
+// The above is to import the Table comp in this file
 
 class App extends Component {
     render() {
+
+        const characters = [
+            {
+              name: 'Charlie',
+              job: 'Janitor',
+            },
+            {
+              name: 'Mac',
+              job: 'Bouncer',
+            },
+            {
+              name: 'Dee',
+              job: 'Aspring actress',
+            },
+            {
+              name: 'Dennis',
+              job: 'Bartender',
+            },
+          ]
+
         return (
             <div className="container">
-                <Table />
+                <Table charParam = {characters} />
             </div>
         )
     }
--- src/Table.js
+++ src/Table.js
@@ -48,35 +48,35 @@ const TableHeader = () => {
     )
   }
 
-  const TableBody = () => {
-    return (
-      <tbody>
-        <tr>
-          <td>Charlie</td>
-          <td>Janitor</td>
-        </tr>
-        <tr>
-          <td>Mac</td>
-          <td>Bouncer</td>
-        </tr>
-        <tr>
-          <td>Dee</td>
-          <td>Aspiring actress</td>
-        </tr>
-        <tr>
-          <td>Dennis</td>
-          <td>Bartender</td>
-        </tr>
-      </tbody>
-    )
+  const TableBody = props => {
+      const rows = props.charData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+            </tr>
+          )
+        }
+      )
+      return <tbody>{rows}</tbody>
+
+//       You'll notice I've added a key index to each table row. You should always use keys when making lists in React, as they help identify each list item. We'll also see how this is necessary in a moment when we want to manipulate list items.
+//       Props are an effective way to pass existing data to a React component, however the component cannot change the props - they're read-only. In the next section, we'll learn how to use state to have further control over handling data in React.
   }
 
   class Table extends Component {
     render() {
+
+        const {charParam} = this.props
+        //const charParam = this.props.charParam
+        
+        // another way of wrtiting what's in the top --> const charParam = this.props.charParam
+        // check the top in inspect/react --> that's the virtual DOM carrying the charParam data in JSON obj format
+
       return (
         <table>
           <TableHeader />
-          <TableBody />
+          <TableBody charData={charParam}/>
         </table>
       )
     }
