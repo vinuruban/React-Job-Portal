df9814c0e0453a7619c9a79b8da55ab0745cdaee
Shantharoopan <vinu.ruban@netbuilder.com>
2019-10-12 13:57:11 +0100

    day 3 - props complete

--- src/Table.js
+++ src/Table.js
@@ -1,42 +1,5 @@
 import React, { Component } from 'react';
 
-// class Table extends Component {
-//     render() {
-//         return (
-//             <table>
-//             <thead>
-//               <tr>
-//                 <th>Name</th>
-//                 <th>Job</th>
-//               </tr>
-//             </thead>
-//             <tbody>
-//               <tr>
-//                 <td>Charlie</td>
-//                 <td>Janitor</td>
-//               </tr>
-//               <tr>
-//                 <td>Mac</td>
-//                 <td>Bouncer</td>
-//               </tr>
-//               <tr>
-//                 <td>Dee</td>
-//                 <td>Aspiring actress</td>
-//               </tr>
-//               <tr>
-//                 <td>Dennis</td>
-//                 <td>Bartender</td>
-//               </tr>
-//             </tbody>
-//           </table>
-//         )
-//     }
-// }
-//
-// export default Table
-
-// The above is long and hardcoded. It doesnt have to be a component - we will change it into functions
-
 const TableHeader = () => {
     return (
       <thead>
@@ -69,7 +32,7 @@ const TableHeader = () => {
 
         const {charParam} = this.props
         //const charParam = this.props.charParam
-        
+
         // another way of wrtiting what's in the top --> const charParam = this.props.charParam
         // check the top in inspect/react --> that's the virtual DOM carrying the charParam data in JSON obj format
 
